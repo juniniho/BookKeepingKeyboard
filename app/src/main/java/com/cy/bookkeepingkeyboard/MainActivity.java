02ec8dbc9de4a6688e27029cc371c8bdbe5ebe97
juniniho <chenyubeijing@163.com>
2019-01-16 17:42:52 +0800

    修改测试布局为相对布局

--- app/src/main/java/com/cy/bookkeepingkeyboard/MainActivity.java
+++ app/src/main/java/com/cy/bookkeepingkeyboard/MainActivity.java
@@ -1,34 +1,23 @@
 package com.cy.bookkeepingkeyboard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.cy.bookingkeepkeyboard.KeyboardUtil;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edt2;
-    private EditText edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edt = findViewById(R.id.edt);
-        edt2 = findViewById(R.id.edt2);
+        final EditText edt2 = findViewById(R.id.edt2);
         final KeyboardUtil keyboardUtil = new KeyboardUtil(this);
-        keyboardUtil.attachTo(edt);
-        edt.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                keyboardUtil.attachTo(edt);
-                return false;
-            }
-        });
+
         edt2.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -36,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
 
     }
 }
